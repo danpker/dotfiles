6328686ee75cc73ee424859533b40694e0ea7318
Daniel Parker <dan@danpker.com>
2024-10-21 12:07:28 +0100

    Updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    fish_add_path /opt/homebrew/bin
+
+    # Set GPG tty
+    set -gx GPG_TTY (tty)
+
+    theme_gruvbox dark
 end
