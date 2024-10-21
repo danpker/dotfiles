d67caa19ecf80f133ebb1decc53f9c85d2c89ff3
Daniel Parker <dan@danpker.com>
2024-10-21 11:48:23 +0100

    Remove help tree sitter

--- vim/.config/nvim/lua/treesitter_options.lua
+++ vim/.config/nvim/lua/treesitter_options.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "python", "lua", "vim", "help", "query", "elixir", "heex", "eex", "hcl", "terraform"},
+  ensure_installed = { "python", "lua", "vim", "query", "elixir", "heex", "eex", "hcl", "terraform"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
